b29e817d0e3dea3195d01b9ca277a63c233071d3
Shaun <shaun@thenetninja.co.uk>
2019-08-14 12:04:13 +0100

    lesson-6

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -5,11 +5,22 @@ void main() => runApp(MaterialApp(
     appBar: AppBar(
       title: Text('my first app'),
       centerTitle: true,
+      backgroundColor: Colors.red[600]
     ),
     body: Center(
-      child: Text('hello, ninjas!'),
+      child: Text(
+        'hello, ninjas!',
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600],
+          fontFamily: 'IndieFlower',
+        ),
+      ),
     ),
     floatingActionButton: FloatingActionButton(
+      backgroundColor: Colors.red[600],
       child: Text('click'),
     ),
   ),
