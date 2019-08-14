e979e8c40e67fe740eb531c4eaecf33a2d2a67c6
Shaun <shaun@thenetninja.co.uk>
2019-08-14 12:25:45 +0100

    lesson-7

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,29 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('my first app'),
-      centerTitle: true,
-      backgroundColor: Colors.red[600]
-    ),
-    body: Center(
-      child: Text(
-        'hello, ninjas!',
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey[600],
-          fontFamily: 'IndieFlower',
+  home: Home(),
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('my first app'),
+          centerTitle: true,
+          backgroundColor: Colors.red[600]
+      ),
+      body: Center(
+        child: Text(
+          'hello again, ninjas!',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower',
+          ),
         ),
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      backgroundColor: Colors.red[600],
-      child: Text('click'),
-    ),
-  ),
-));
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red[600],
+        child: Text('click'),
+      ),
+    );
+  }
+}
+
 
 
