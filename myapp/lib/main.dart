dd1932f9f58e99d690982499315543bebbd160b2
Shaun <shaun@thenetninja.co.uk>
2019-08-14 11:11:00 +0100

    lesson-5

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text('hey ninjas!!')
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('my first app'),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Text('hello, ninjas!'),
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Text('click'),
+    ),
+  ),
 ));
 
 
