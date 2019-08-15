34a56f4e1de20eb1450a1438a024d4adeacf195f
Shaun <shaun@thenetninja.co.uk>
2019-08-15 09:38:15 +0100

    lesson-18

--- quotes/lib/main.dart
+++ quotes/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 void main() => runApp(MaterialApp(
   home: QuoteList()
@@ -11,10 +12,16 @@ class QuoteList extends StatefulWidget {
 
 class _QuoteListState extends State<QuoteList> {
 
-  List<String> quotes = [
-    'Be yourself; everyone else is already taken',
-    'I have nothing to declare except my genius',
-    'The truth is rarely pure and never simple'
+  //  List<String> quotes = [
+  //    'Be yourself; everyone else is already taken',
+  //    'I have nothing to declare except my genius',
+  //    'The truth is rarely pure and never simple'
+  //  ];
+
+  List<Quote> quotes = [
+    Quote(author: 'Oscar Wilde', text: 'Be yourself; everyone else is already taken'),
+    Quote(author: 'Oscar Wilde', text: 'I have nothing to declare except my genius'),
+    Quote(author: 'Oscar Wilde', text: 'The truth is rarely pure and never simple')
   ];
 
   @override
@@ -27,7 +34,7 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => Text(quote)).toList(),
+        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
       ),
     );
   }
---
+++ quotes/lib/quote.dart
@@ -0,0 +1,25 @@
+class Quote {
+
+  String text;
+  String author;
+
+  //  normal constructor, as we've already seen
+
+  //  Quote(String author, String text){
+  //    this.text = text;
+  //    this.author = author;
+  //  }
+
+  //  constructor with named parameters
+
+  //  Quote({ String author, String text }){
+  //    this.text = text;
+  //    this.author = author;
+  //  }
+
+  // constructor with named parameters
+  // & automatically assigns named arguments to class properties
+
+  Quote({ this.text, this.author });
+
+}
