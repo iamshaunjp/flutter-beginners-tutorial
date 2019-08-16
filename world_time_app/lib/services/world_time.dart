82a42b1f9b2f7a15e1b319c2677947c64bc9b69f
Shaun <shaun@thenetninja.co.uk>
2019-08-16 10:35:44 +0100

    lesson-29

--- world_time_app/lib/services/world_time.dart
+++ world_time_app/lib/services/world_time.dart
@@ -11,24 +11,28 @@ class WorldTime {
   WorldTime({ this.location, this.flag, this.url });
 
   Future<void> getTime() async {
-    // make the request
-    Response response = await get('http://worldtimeapi.org/api/timezone/$url');
-    Map data = jsonDecode(response.body);
 
-    // get properties from json
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1,3);
+    try{
+      // make the request
+      Response response = await get('http://worldtimeapi.org/api/timezone/$url');
+      Map data = jsonDecode(response.body);
 
-    // create DateTime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
+      // get properties from json
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1,3);
 
-    print(now);
+      // create DateTime object
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
 
-    // set the time property
-    time = now.toString();
+      // set the time property
+      time = now.toString();
+    }
+    catch (e) {
+      print(e);
+      time = 'could not get time';
+    }
+
   }
 
 }
-
-// WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
