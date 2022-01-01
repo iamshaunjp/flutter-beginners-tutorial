e986d5d3120092bdc79eac892bb61d3847053354
Naswih <51323070+naxwh@users.noreply.github.com>
2022-01-01 21:27:59 +0530

    Update loading.dart

--- world_time_app/lib/pages/loading.dart
+++ world_time_app/lib/pages/loading.dart
@@ -11,7 +11,7 @@ class _LoadingState extends State<Loading> {
 
   void getData() async {
 
-    Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
+    Response response = await get(Uri.http('jsonplaceholder.typicode.com', '/todos/1' ));
     // print(response.body);
     Map data = jsonDecode(response.body);
     print(data);
